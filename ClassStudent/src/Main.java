bb9153ac1a40ed174afbcaf5dc165ef8aeb3ef9e
CasualCross <141323149+CasualCross@users.noreply.github.com>
2024-09-20 15:39:24 +0600

    Add files via upload

--- ClassStudent/src/Main.java
+++ ClassStudent/src/Main.java
@@ -4,26 +4,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Student student1 = new Student();
-        student1.name = "Asanov Erlan";
-        student1.department = "Cybersecurity";
-        student1.studentID = 101101;
-        student1.GPA = 70;
-        student1.age = 19;
-
-        Student student2 = new Student();
-        student2.name = "Gambino Chiquita";
-        student2.department = "Computer Science";
-        student2.studentID = 909909;
-        student2.GPA = 75;
-        student2.age = 20;
-
-        Student student3 = new Student();
-        student3.name = "Sabre Lamarr";
-        student3.department = "Martial Arts";
-        student3.studentID = 808808;
-        student3.GPA = 80;
-        student3.age = 21;
+        Student student1 = new Student("Asanov Erlan", "Cybersecurity", 101101, 70, 19);
+        Student student2 = new Student("Gambino Chiquita", "Computer Science", 909909, 75, 20);
+        Student student3 = new Student("Sabre Lamarr", "Martial Arts", 808808, 80, 21);
 
         student1.displayInfo();
         System.out.println(" ");
@@ -75,22 +58,22 @@ public class Main
         scanner.nextLine();
         System.out.println("Enter new name: ");
         String newName = scanner.nextLine();
-        student.changeName(newName);
+        student.setName(newName);
 
         System.out.println("Enter new department: ");
         String newDep = scanner.nextLine();
-        student.changeDepartment(newDep);
+        student.setDepartment(newDep);
 
         System.out.println("Enter new age: ");
         int newAge = scanner.nextInt();
-        student.changeAge(newAge);
+        student.setAge(newAge);
 
         System.out.println("Enter new GPA: ");
         int newGPA = scanner.nextInt();
-        student.changeGPA(newGPA);
+        student.setGPA(newGPA);
 
         System.out.println("Enter new Student ID: ");
         int newID = scanner.nextInt();
-        student.changeID(newID);
+        student.setStudentID(newID);
     }
 }
--- ClassStudent/src/Student.java
+++ ClassStudent/src/Student.java
@@ -1,47 +1,100 @@
 public class Student
 {
-    public String name;
-    public String department;
-    public int age;
-    public int studentID;
-    public int GPA;
+    private String name;
+    private String department;
+    private int age;
+    private int studentID;
+    private int GPA;
 
-    //public Student(String name, String department, int studentID, int GPA, int age)
-    //{
-    //    this.name = name;
-    //    this.department = department;
-    //    this.studentID = studentID;
-    //    this.GPA = GPA;
-    //    this.age = age;
-    //}
+    public Student(String name, String department, int studentID, int GPA, int age)
+    {
+        this.name = name;
+        this.department = department;
+        this.studentID = studentID;
+        this.GPA = GPA;
+        this.age = age;
+    }
 
     public void displayInfo()
     {
         System.out.println("Student Name: " + name);
-        System.out.println("Student ID: " + studentID);
+        System.out.println("Student ID: " + getStudentID());
         System.out.println("Student Department: " + department);
         System.out.println("Student GPA: " + GPA);
         System.out.println("Student Age: " + age);
     }
 
-    public void changeName(String newName)
+    public String getName()
+    {
+        return name;
+    }
+    public String getDepartment()
+    {
+        return department;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public int getGpa()
+    {
+        return GPA;
+    }
+    public String getStudentID()
+    {
+        if(studentID > 99)
+        {
+            return String.valueOf(studentID);
+        }
+        else if(studentID > 9)
+        {
+            return String.format("%03d", studentID);
+        }
+        else
+        {
+            return String.format("%02d", studentID);
+        }
+    }
+
+    public void setName(String newName)
     {
         this.name = newName;
     }
-    public void changeID (int newID)
+    public void setDepartment(String newDepartment)
     {
-        this.studentID = newID;
+        this.department = newDepartment;
     }
-    public void changeDepartment(String newDep)
+    public void setAge(int newAge)
     {
-        this.department = newDep;
+        if(newAge >= 18)
+        {
+            this.age = newAge;
+        }
+        else
+        {
+            System.out.println("Error! Age should be from 18+");
+        }
     }
-    public void changeGPA (int newGPA)
+    public void setStudentID(int newStudentID)
     {
-        this.GPA = newGPA;
+        if(newStudentID >= 1 && newStudentID <= 999)
+        {
+            this.studentID = newStudentID;
+        }
+        else
+        {
+            System.out.println("Error! StudentID should be in range: 1-999");
+        }
     }
-    public void changeAge (int newAge)
+    public void setGPA(int newGPA)
     {
-        this.age = newAge;
+        if(newGPA >= 1 && newGPA <= 100)
+        {
+            this.GPA = newGPA;
+        }
+        else
+        {
+            System.out.println("Error! GPA should be 1-100");
+        }
     }
 }
